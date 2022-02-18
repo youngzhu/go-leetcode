dd74f71329ac1ead9a1d2dbda4a0b4a792048294
young-win <young.zhu.2012@gmail.com>
2022-02-18 13:41:51 +0800

    template

--- p000/template.go
+++ p000/template.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+)
+
 /*
 
  */
+var testCases = []struct {
+	nums   []int
+	k      int
+	result int
+}{
+	{[]int{2, 1}, 2, 1},
+}
+
+func main() {
+	for _, tc := range testCases {
+		got := 0
+		want := tc.result
+		if got != want {
+			fmt.Printf("got: %d, want: %d\n", got, want)
+		} else {
+			fmt.Println("ok")
+		}
+	}
+
+}
